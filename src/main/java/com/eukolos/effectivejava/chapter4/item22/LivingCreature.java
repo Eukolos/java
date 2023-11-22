f10e2803d6d2b35936460e44620a190dc325a207
eukolos <eminaksoy35@gmail.com>
2023-11-22 16:28:03 +0300

    interfaces only to define types

---
+++ src/main/java/com/eukolos/effectivejava/chapter4/item22/LivingCreature.java
@@ -0,0 +1,6 @@
+package com.eukolos.effectivejava.chapter4.item22;
+
+public interface LivingCreature {
+    // dont have to add public static final
+    boolean isAlive = true;
+}
---
+++ src/main/java/com/eukolos/effectivejava/chapter4/item22/Main.java
@@ -0,0 +1,12 @@
+package com.eukolos.effectivejava.chapter4.item22;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(PhysicalConstants.AVOGADROS_NUMBER);
+        System.out.println(PhysicalConstants.BOLTZMANN_CONSTANT);
+        System.out.println(PhysicalConstants.ELECTRON_MASS);
+        System.out.println(LivingCreature.isAlive);
+    }
+}

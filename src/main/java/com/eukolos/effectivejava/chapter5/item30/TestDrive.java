17c9287c284767a06c2c2be28b2963db39de92c2
Eukolos <eminaksoy35@gmail.com>
2023-11-26 22:01:22 +0300

    generic methods

---
+++ src/main/java/com/eukolos/effectivejava/chapter5/item30/TestDrive.java
@@ -0,0 +1,17 @@
+package com.eukolos.effectivejava.chapter5.item30;
+
+import java.util.Collections;
+
+public class TestDrive {
+
+    public static void main(String[] args) {
+
+        Collections.sort(null);
+
+        // Değiştirilemez ama bir çok türle kullanılır!
+        // reverseOrder
+        Collections.emptySet();
+
+    }
+
+}
